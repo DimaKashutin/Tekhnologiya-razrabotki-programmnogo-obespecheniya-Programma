6f89646b84b7853fd15f8e2c15452d5547ceb6f4
DimaKashutin <149133008+DimaKashutin@users.noreply.github.com>
2024-06-29 10:03:19 +0500

    Технология разработки ПО(Сессия)
    Это файлы проекта из Visual Studio.
    Кашутин и Зайниев делали совместную работу на тему (Крестики Нолики).

---
+++ xo/xo/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xo
+{
+    public partial class MainWindow : Window
+    {
+        private TicTacToe game;
+        private Button[,] buttons;
+        private int boardSize;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeGameBoard(int size)
+        {
+            gameBoard.Children.Clear();
+            gameBoard.Rows = size;
+            gameBoard.Columns = size;
+
+            buttons = new Button[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button button = new Button();
+                    button.FontSize = 32;
+                    button.Click += Button_Click;
+                    buttons[i, j] = button;
+                    gameBoard.Children.Add(button);
+                }
+            }
+        }
+
+        private void btnPlayerVsBot_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame(true);
+        }
+
+        private void btnPlayerVsPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame(false);
+        }
+
+        private void StartGame(bool isBotGame)
+        {
+            if (int.TryParse(txtBoardSize.Text, out boardSize) && boardSize >= 3)
+            {
+                game = new TicTacToe('X', 'O', isBotGame, boardSize);
+                InitializeGameBoard(boardSize);
+                ClearBoard();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid board size (minimum 3).");
+            }
+        }
+
+        private void ClearBoard()
+        {
+            foreach (Button button in buttons)
+            {
+                button.Content = null;
+                button.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            // Find the row and column of the clicked button
+            int row = -1, col = -1;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (buttons[i, j] == clickedButton)
+                    {
+                        row = i;
+                        col = j;
+                        break;
+                    }
+                }
+                if (row != -1) break;
+            }
+
+            if (game.MakeMove(row, col))
+            {
+                clickedButton.Content = game.CurrentPlayerSymbol;
+                clickedButton.IsEnabled = false;
+
+                if (game.CheckWinner(game.CurrentPlayerSymbol))
+                {
+                    MessageBox.Show($"Player {game.CurrentPlayerSymbol} wins!");
+                    ClearBoard();
+                    return;
+                }
+
+                game.SwitchPlayer();
+
+                if (game.IsBotGame && game.CurrentPlayerSymbol == game.BotSymbol)
+                {
+                    (int botRow, int botCol) = game.MakeBotMove();
+                    buttons[botRow, botCol].Content = game.BotSymbol;
+                    buttons[botRow, botCol].IsEnabled = false;
+
+                    if (game.CheckWinner(game.BotSymbol))
+                    {
+                        MessageBox.Show("Bot wins!");
+                        ClearBoard();
+                        return;
+                    }
+
+                    game.SwitchPlayer();
+                }
+            }
+        }
+    }
+}
---
+++ xo/xo/TicTacToe.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xo
+{
+    public class TicTacToe
+    {
+        private char[,] board;
+        public char PlayerSymbol { get; }
+        public char BotSymbol { get; }
+        public char CurrentPlayerSymbol { get; private set; }
+        public bool IsBotGame { get; }
+        private int boardSize;
+        private int winCondition;
+
+        public TicTacToe(char playerSymbol, char botSymbol, bool isBotGame, int size)
+        {
+            board = new char[size, size];
+            PlayerSymbol = playerSymbol;
+            BotSymbol = botSymbol;
+            IsBotGame = isBotGame;
+            CurrentPlayerSymbol = PlayerSymbol;
+            boardSize = size;
+            winCondition = boardSize >= 4 ? 4 : 3;
+        }
+
+        public bool MakeMove(int row, int col)
+        {
+            if (board[row, col] == '\0')
+            {
+                board[row, col] = CurrentPlayerSymbol;
+                return true;
+            }
+            return false;
+        }
+
+        public void SwitchPlayer()
+        {
+            CurrentPlayerSymbol = CurrentPlayerSymbol == PlayerSymbol ? BotSymbol : PlayerSymbol;
+        }
+
+        public bool CheckWinner(char symbol)
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (CheckRow(i, symbol) || CheckColumn(i, symbol))
+                    return true;
+            }
+            return CheckDiagonals(symbol);
+        }
+
+        private bool CheckRow(int row, char symbol)
+        {
+            int count = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (board[row, i] == symbol)
+                {
+                    count++;
+                    if (count == winCondition)
+                        return true;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckColumn(int col, char symbol)
+        {
+            int count = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (board[i, col] == symbol)
+                {
+                    count++;
+                    if (count == winCondition)
+                        return true;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckDiagonals(char symbol)
+        {
+            for (int i = 0; i <= boardSize - winCondition; i++)
+            {
+                for (int j = 0; j <= boardSize - winCondition; j++)
+                {
+                    if (CheckDiagonalFromPosition(i, j, symbol))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckDiagonalFromPosition(int row, int col, char symbol)
+        {
+            // Check descending diagonal
+            int count = 0;
+            for (int i = 0; i < winCondition; i++)
+            {
+                if (board[row + i, col + i] == symbol)
+                {
+                    count++;
+                    if (count == winCondition)
+                        return true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Check ascending diagonal
+            count = 0;
+            for (int i = 0; i < winCondition; i++)
+            {
+                if (board[row + winCondition - 1 - i, col + i] == symbol)
+                {
+                    count++;
+                    if (count == winCondition)
+                        return true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return false;
+        }
+
+        public (int, int) MakeBotMove()
+        {
+            Random rnd = new Random();
+            int row, col;
+            do
+            {
+                row = rnd.Next(boardSize);
+                col = rnd.Next(boardSize);
+            }
+            while (board[row, col] != '\0');
+
+            board[row, col] = BotSymbol;
+            return (row, col);
+        }
+    }
+}
